796e400579363e0ced08f2c46a567032e579650b
Даниил Гончаренко <GoncharenkoDaniil0@yandex.ru>
2024-03-24 16:35:18 +0300

    remove import reverse from blog/models.py

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.urls import reverse
 
 from core.models import PublishedModel
 from .constant import MAX_LENGTH
